6c53ce742ac91d8b8856906cb590a34a8fff19a6
Aleksey <sphota9@gmail.com>
2017-10-07 15:30:33 -0400

    clean up logic in __build_ctree_internal()

--- density-control-io/Calibrator.hpp
+++ density-control-io/Calibrator.hpp
@@ -15,7 +15,7 @@ template <typename T>
 class CNode
 {
  public:
-    uint density;
+    float density;
     std::pair<T, T> addr_range;
     std::shared_ptr<CNode> left, right;
     CNode(uint d) : density(d), left(nullptr), right(nullptr) {}
@@ -34,12 +34,13 @@ private:
     std::vector<T> __data;
     std::shared_ptr<CNode<T>> __root = nullptr;
     
-    int __density_of_range(std::pair<T, T> addr_range);
+    float __density_of_range(std::pair<T, T> addr_range);
     void __build_ctree_internal(std::shared_ptr<CNode<T>> __root);
     void __build_ctree();
     
 public:
     
+    /* ctor */
     Calibrator
     (std::vector<T> & set, const int & N, int M, std::vector<int> & d, int D) :
     __data(set),
@@ -64,20 +65,22 @@ void Calibrator<T>::build()
 #pragma mark - private
 
 template <typename T>
-int Calibrator<T>::__density_of_range(std::pair<T, T> range)
+float Calibrator<T>::__density_of_range(std::pair<T, T> range)
 {
-    int density = -1;
+    float density = -1.0;
     if (range.first == range.second) {
         return __sizes_of_pages[range.first];
     }
     
     int i = range.first;
     int sum = 0;
-    for (; i <= range.second; ++i) {
+    int count = 0;
+    for (; i <= range.second; i++) {
+        count++;
         sum += __sizes_of_pages[i];
     }
     
-    density = sum / i;
+    density = (float)sum / (float)count;
     return density;
 }
 
@@ -86,7 +89,7 @@ void Calibrator<T>::__build_ctree()
 {
     std::pair<T, T> root_range = {0, __page_count - 1};
     if (!__root) {
-        int density = __density_of_range(root_range);
+        float density = __density_of_range(root_range);
         std::cout << density << std::endl;
         __root = std::make_shared<CNode<T>>(density);
         __root->addr_range = root_range;
@@ -97,23 +100,25 @@ void Calibrator<T>::__build_ctree()
 template <typename T>
 void Calibrator<T>::__build_ctree_internal(std::shared_ptr<CNode<T>> __root)
 {
-    if (__root->addr_range.first == __root->addr_range.second) {
-        __root->density = __density_of_range(__root->addr_range);
+    if (__root->addr_range.first == __root->addr_range.second)
         return;
-    }
     
-    std::shared_ptr<CNode<T>> right_child = std::make_shared<CNode<T>>(-1),
+    std::shared_ptr<CNode<T>>
+    right_child = std::make_shared<CNode<T>>(-1),
     left_child = std::make_shared<CNode<T>>(-1);
     
-    right_child->addr_range.first = __root->addr_range.first;
-    right_child->addr_range.second = (__root->addr_range.first + __root->addr_range.second) / 2;
-    right_child->density = __density_of_range(right_child->addr_range);
-    __build_ctree_internal(right_child);
-    
-    left_child->addr_range.first = ((__root->addr_range.first + __root->addr_range.second) / 2) + 1;
-    left_child->addr_range.second = __root->addr_range.second;
+    left_child->addr_range.first = __root->addr_range.first;
+    left_child->addr_range.second = (__root->addr_range.first + __root->addr_range.second) / 2;
     left_child->density = __density_of_range(left_child->addr_range);
-    __build_ctree_internal(left_child);
+    __root->left = left_child;
+    
+    right_child->addr_range.first = ((__root->addr_range.first + __root->addr_range.second) / 2) + 1;
+    right_child->addr_range.second = __root->addr_range.second;
+    right_child->density = __density_of_range(right_child->addr_range);
+    __root->right = right_child;
+    
+    __build_ctree_internal(__root->left);
+    __build_ctree_internal(__root->right);
 }
 
 
--- density-control-io/main.cpp
+++ density-control-io/main.cpp
@@ -12,16 +12,25 @@
 int main(int argc, const char * argv[]) {
 
     // we use indeces as addresses, so the type is 'int'
-    std::vector<int> v = {1, 2, 3, 4, 7, 9,
-                         11, 13, 17, 13,
-                         19, 25,
-                         28, 29, 30, 31, 32, 39};
+    std::vector<int> v = {1, 2, 3,
+                         11, 13,
+                         19,
+                         28, 29};
     
     /* page sizes */
-    std::vector<int> d = {6, 4, 2, 6};
+    std::vector<int> d = {3, 2, 1, 2};
     
-    /* N = 18, M = 4, D = 6 */
-    Calibrator<int> calibrator = Calibrator<int>(v, 18, 4, d, 6);
+    // we use indeces as addresses, so the type is 'int'
+    std::vector<int> v1 = {1, 2, 3, 4, 7, 9,
+        11, 13, 17, 13,
+        19, 25,
+        28, 29, 30, 31, 32, 39};
+    
+    /* page sizes */
+    std::vector<int> d1 = {6, 4, 2, 6};
+    
+    /* N = 8, M = 4, D = 6 */
+    Calibrator<int> calibrator = Calibrator<int>(v, 8, 4, d, 3);
     calibrator.build();
     return 0;
 }
