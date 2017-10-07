457e30bc557405bc6ab47027df3fbbc0b1fa01df
Aleksey <sphota9@gmail.com>
2017-10-07 02:46:31 -0400

    add some comments

--- density-control-io/main.cpp
+++ density-control-io/main.cpp
@@ -17,8 +17,10 @@ int main(int argc, const char * argv[]) {
                          19, 25,
                          28, 29, 30, 31, 32, 39};
     
+    /* page sizes */
     std::vector<int> d = {6, 4, 2, 6};
     
+    /* N = 18, M = 4, D = 6 */
     Calibrator<int> calibrator = Calibrator<int>(v, 18, 4, d, 6);
     calibrator.build();
     return 0;
