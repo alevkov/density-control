59292c3c1a4ba5c01ca85f921f375655226a82f3
Aleksey <sphota9@gmail.com>
2017-10-08 18:03:31 -0400

    organize main file

--- density-control-io/main.cpp
+++ density-control-io/main.cpp
@@ -11,26 +11,35 @@
 
 int main(int argc, const char * argv[]) {
 
-    // we use indeces as addresses, so the type is 'int'
     std::vector<int> v = {1, 2, 3,
                          11, 13,
                          19,
                          28, 29};
-    
     /* page sizes */
     std::vector<int> d = {3, 2, 1, 2};
+    int D = 3;
     
-    // we use indeces as addresses, so the type is 'int'
+    /* we use indeces as addresses, so the type is 'int' */
     std::vector<int> v1 = {1, 2, 3, 4, 7, 9,
         11, 13, 17, 13,
         19, 25,
         28, 29, 30, 31, 32, 39};
-    
-    /* page sizes */
     std::vector<int> d1 = {6, 4, 2, 6};
+    int D1 = 6;
     
-    /* N = 8, M = 4, D = 6 */
-    Calibrator<int, int> calibrator = Calibrator<int, int>(v, 8, 4, d, 3);
+    /**
+     D - d = 1 (minimum page size)
+     2 + 0
+     2 + (1 + 1 - 1)(1) = 3
+     2 + (2 + 1 - 1)(1) = 4
+     */
+    
+    std::vector<int> file = v;
+    std::vector<int> page_sizes = d;
+    int max_per_page = D;
+    
+    Calibrator<int, int> calibrator = Calibrator<int, int>(file, (int)file.size(), (int)page_sizes.size(), page_sizes, max_per_page);
     calibrator.build();
+    
     return 0;
 }
